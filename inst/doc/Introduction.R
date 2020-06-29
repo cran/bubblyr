5e8bfed3ec79c7933a17dcddee7df6bee94b6096
Mohamed El Fodil Ihaddaden <ihaddaden.fodeil@gmail.com>
2020-06-29 08:20:10 +0000

    version 0.1.0

---
+++ inst/doc/Introduction.R
@@ -0,0 +1,76 @@
+## ---- eval=FALSE--------------------------------------------------------------
+#  
+#  install.packages("remotes") #if not installed
+#  
+#  remotes::install_github("feddelegrand7/bubblyr")
+#  
+
+## ---- echo=FALSE--------------------------------------------------------------
+list <- c(
+    "ocean",
+    "cherry",
+    "hippie",
+    "bigmom",
+    "deepsea",
+    "illusion",
+    "rstudio",
+    "ash",
+    "classy",
+    "volcano",
+    "lacoste",
+    "warmup",
+    "fire",
+    "traffic",
+    "life",
+    "darksky",
+    "orangina",
+    "meteor"
+  )
+
+knitr::kable(list, col.names = "Themes")
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  library(shiny)
+#  library(bubblyr)
+#  
+#  ui <- fluidPage(
+#  
+#      bubbly(theme = "bigmom")
+#  
+#  )
+#  
+#  server <- function(input, output) {}
+#  
+#  shinyApp(ui = ui, server = server)
+#  
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  library(shiny)
+#  library(bubblyr)
+#  
+#  ui <- fluidPage(
+#  
+#      bubbly(theme = "rstudio")
+#  
+#  )
+#  
+#  server <- function(input, output) {}
+#  
+#  shinyApp(ui = ui, server = server)
+#  
+
+## ---- eval=FALSE--------------------------------------------------------------
+#  library(shiny)
+#  library(bubblyr)
+#  
+#  ui <- fluidPage(
+#  
+#      bubbly(theme = "classy")
+#  
+#  )
+#  
+#  server <- function(input, output) {}
+#  
+#  shinyApp(ui = ui, server = server)
+#  
+
